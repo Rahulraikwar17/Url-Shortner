da686fad6dbc4a2de6c7ac15226623b4e0df9388
rahul raikwar <you@example.com>
2026-09-20 22:33:35 +0530

    port added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import { connectDb } from "./config/db.js";
 
 await connectDb();
 
-app.listen(3000, () => {
-  console.log("server started at port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
